648de3cc52ffc37afeec4b8c66324d5bb08721aa
yonatan afewerk <yonatanafewerk@gmail.com>
2023-11-01 10:21:07 +0300

    UI: minim ui design complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 import "./App.css";
 
 function App() {
+    const emoji_array = [
+        "😀",
+        "😀",
+        "😁",
+        "😁",
+        "😂",
+        "😂",
+        "🤣",
+        "🤣",
+        "😃",
+        "😃",
+        "😄",
+        "😄",
+    ];
     return (
         <>
+            <div className="box">
+                {emoji_array.map((emo, key) => (
+                    <div className="card" key={key}>
+                        <h2>{emo}</h2>
+                    </div>
+                ))}
+            </div>
             <h1>CONCENTRATION</h1>
         </>
     );
